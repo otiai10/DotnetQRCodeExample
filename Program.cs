91c1133f7a73a1163fe5497f326d81b54d68d7f7
Hiromu OCHIAI <otiai10@gmail.com>
2020-11-17 21:16:46 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using ZXing;
+
+namespace QRExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello QR Code!");
+            BarcodeWriter writer = new BarcodeWriter
+            {
+                Format = BarcodeFormat.QR_CODE,
+                Options = new ZXing.QrCode.QrCodeEncodingOptions
+                {
+                    CharacterSet = "UTF-8",
+                    Height = 400,
+                    Width = 400
+                }
+            };
+            string body = @"https://otiai10.hatenablog.com/";
+            System.Drawing.Bitmap bitmap = writer.Write(body);
+            bitmap.Save("./example.png", System.Drawing.Imaging.ImageFormat.Png);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+```zsh
+git clone git@github.com:otiai10/DotnetQRCodeExample.git QRCodeExample
+cd QRCodeExample
+dotnet run
+```
